dc0fae8b3e6254e9f182e4d561321cb95fa69123
alpanayotov <alexander.panayotov@gmail.com>
2016-08-12 16:31:45 +0300

    Add inifinite scroll to search results

--- www/js/search/search.controller.js
+++ www/js/search/search.controller.js
@@ -5,16 +5,21 @@
 		.module('app.core')
 		.controller('SearchCtrl', SearchCtrl);
 
-	SearchCtrl.$inject = ['$ionicPopup', 'searchService', 'searchSettingsService'];
+	SearchCtrl.$inject = ['$scope', '$ionicPopup', '$ionicLoading', 'searchService', 'searchSettingsService'];
 
-	function SearchCtrl($ionicPopup, searchService, searchSettingsService) {
+	function SearchCtrl($scope, $ionicPopup, $ionicLoading, searchService, searchSettingsService) {
 		var vm = this;
 		
-		vm.showSettings = false;
-		vm.settings     = {};
-		vm.phrase       = '';
-		vm.results      = {};
-		vm.search       = search;
+		vm.settings    = {};
+		vm.phrase      = '';
+		vm.results     = {
+			entries: []
+		};
+		vm.requestPage = 1;
+		vm.pages       = 1;
+		vm.loadMore    = false;
+		vm.search      = search;
+		vm.load        = load;
 
 		function search() {
 			if ( !vm.phrase ) {
@@ -25,23 +30,66 @@
 
 				return;
 			}
-			vm.settings   = getSettings();	
-			vm.settings.s = vm.phrase;
+			
+			vm.requestPage   = 1;
+			vm.settings      = getSettings();	
+			vm.settings.s    = vm.phrase;
+			vm.settings.page = vm.requestPage;
+
+			$ionicLoading.show({
+				template: 'Searching...'
+			});
 
 			searchService.search(vm.settings).then( function(payload) {
-				if ( payload.Response === 'True' ) {
-					vm.results.success = true;
-					vm.results.entries = payload.Search;
-					vm.results.error   = '';
-				} else {
-					vm.results.success = false;
-					vm.results.error   = payload.Error;
-				}
+				contentLoaded(payload, true);
+				$ionicLoading.hide();
 			});
+		};
+
+		function load() {
+			if ( vm.requestPage > vm.pages ) {
+				vm.loadMore = false;
+				return;
+			}
+
+			vm.settings.page = vm.requestPage;
+
+			searchService.search(vm.settings).then( function(payload){
+				contentLoaded(payload, false);
+				$scope.$broadcast('scroll.infiniteScrollComplete');
+			});
+
+		};
+
+		function contentLoaded(payload, initialRequest){
+
+			if ( payload.Response === 'True' ) {
+				if ( initialRequest ) {
+					vm.results.entries = [];
+
+					if ( parseInt(payload.totalResults) > 10 ) {
+						vm.pages    = Math.ceil( payload.totalResults / 10 );
+						vm.loadMore = true;
+						vm.requestPage++; 
+					}
+				} else {
+					vm.requestPage++;	
+				}
+
+				vm.results.success = true;
+				vm.results.entries = vm.results.entries.concat(payload.Search);
+				vm.results.error   = '';
+			} else {
+
+				vm.results.success = false;
+				vm.results.error   = payload.Error;
+				vm.loadMore        = false;
+			}
+			
 		};
 
 		function getSettings(){
 			return searchSettingsService.get();
-		}
+		};
 	}
 })();
