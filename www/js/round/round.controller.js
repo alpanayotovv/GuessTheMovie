d7a5f1e82e5177ef0833d5313b2dc28fa4a7e7c4
alpanayotov <alexander.panayotov@gmail.com>
2016-09-01 09:31:58 +0300

    Add points to a team automatically on guessed movie

--- www/js/round/round.controller.js
+++ www/js/round/round.controller.js
@@ -5,18 +5,19 @@
 		.module('app.core')
 		.controller('RoundCtrl', RoundCtrl);
 
-	RoundCtrl.$inject = ['$scope', '$state', '$ionicScrollDelegate', '$ionicPopup', 'roundService'];
+	RoundCtrl.$inject = ['$scope', '$state', '$ionicScrollDelegate', '$ionicPopup', 'roundService', 'teamService'];
 
-	function RoundCtrl($scope, $state, $ionicScrollDelegate, $ionicPopup, roundService) {
+	function RoundCtrl($scope, $state, $ionicScrollDelegate, $ionicPopup, roundService, teamService) {
 		var vm    = this;
 		
 		vm.movie       = false;
 		vm.settings    = {};
 		vm.timeLeft    = '';
+		vm.roundStatus = 'new';
+		vm.team        = {};
 		vm.start       = start;
 		vm.pause       = pause;
 		vm.started     = false;
-		vm.roundStatus = 'new';
 		vm.reset       = reset;
 		vm.end         = end;
 
@@ -42,6 +43,13 @@
 			roundService.getSettings().then( function(settings) {
 				vm.settings = settings;
 				vm.timeLeft = settings.time * 60;
+
+				if ( vm.settings.team ) {
+					teamService.getTeam(vm.settings.team).then( function(team){
+						vm.team = team;
+						vm.team.index = vm.settings.team;
+					});
+				}
 			});
 		};
 
@@ -62,11 +70,13 @@
 
 			$ionicPopup.alert({
 				title: 'Round Won!',
-				content: 'Congratulations! You just won ' + vm.settings.points  + ' points! Add them to your team!'
+				content: 'Congratulations! You just won ' + vm.settings.points  + ' points! They were added to your team!'
 			});
 
-			roundService.reset();
+			var points = parseInt( vm.team.score, 10 ) + parseInt( vm.settings.points, 10 );
 
+			teamService.updateTeam( vm.team.name, points, vm.team.index );
+			roundService.reset();
 			$state.go('app.scores');
 		};
 
